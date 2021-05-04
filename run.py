2f73b1d6ed25ea88a260f7044e865a8df0940bce
emmasarge <emmasargeant206@gmail.com>
2021-05-04 17:03:16 +0000

    post email contact.html

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"): 
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -26,8 +31,11 @@ def about_products(products_name):
     return render_template("products.html", products=products)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
