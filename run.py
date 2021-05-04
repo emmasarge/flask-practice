3bacbd2daed9175ed5c759ddadc569f1917c9f18
emmasarge <emmasargeant206@gmail.com>
2021-05-04 11:32:33 +0000

    routed contact.html to run.py

--- run.py
+++ run.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(
