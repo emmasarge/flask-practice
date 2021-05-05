ed018ff5053c3064bc9249f08eee9c9d9f48cd9d
emmasarge <emmasargeant206@gmail.com>
2021-05-05 11:03:28 +0000

    changes

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
-
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -43,7 +44,9 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("GET", "5000")),
+        debug=False
+        )
